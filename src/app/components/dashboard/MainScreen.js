238d69aae35cd791d463c5521821a8e87201516b
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-05-04 17:47:03 +0530

    working on header section

--- src/app/components/dashboard/MainScreen.js
+++ src/app/components/dashboard/MainScreen.js
@@ -4,12 +4,15 @@ import Image from "next/image";
 
 function MainScreen() {
   return (
-    <div className="bg-mainColor px-10">
+    <div className="bg-mainColor px-10 py-10">
       <div>
-        <div className=" flex justify-end text-white">
-          <p className="text-start">Seqarchbar</p>
-          <p className="">messge</p>
-          <p>profile</p>
+        <div className="flex justify-end text-white">
+          <input
+            className="px-5 text-sm border-b-2 bg-transparent w-250 bg-mainColor border-b-gray outline-none opacity-50 focus:border-green"
+            placeholder="Enter your search request.."
+          />
+          <p className="">m</p>
+          <p>p</p>
         </div>
       </div>
 
@@ -21,7 +24,7 @@ function MainScreen() {
           alt="movies"
           className="h-96 w-auto m-5 p-5"
         />
-      </div> 
+      </div>
       <div>
         <div className="text-left py-3 text-white">Upcoming Movies</div>
         <div className="flex justify-center items-center text-left p-5 gap-3">
